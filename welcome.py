d6691872a4f0a349bfccbc063620af93e6cbb4ce
klarahei <141242034+klarahei@users.noreply.github.com>
2023-08-03 12:35:58 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,33 +1,32 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
-#import numpy as np
 import os
 from bokeh.plotting import figure
 
 file_name_list = []
 for i in os.listdir():
- if i.endswith('csv'):
-    file_name_list.append(i)
-  
+    if i.endswith('csv'):
+        file_name_list.append(i)
+
 st.write(file_name_list)
 
 df = pd.read_csv('Galapagos Islands.csv')
 st.dataframe(df)
 
-
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select x-element', el_list)
 y_axis = st.selectbox('select y-element', el_list)
 
-st.multiselect('select location', file_name_list)
-#
-colorlist = ['red','green','pink','yellow','blue']
-p = figure(x_axis_label = x_axis + '(wt.%)',y_axis_label = y_axis + '(wt.%)')
-for i in range(len(file_name_list)):
- df1 = pd.read_csv(file_name_list[i])
- p.circle(df1[x_axis]/10000, df1[y_axis]/10000, color=colorlist[i])
+selected_files = st.multiselect('select location', file_name_list)
 
-#show(p)
+colorlist = ['red', 'green', 'pink', 'yellow', 'blue']
+plots = []
 
-st.bokeh_chart(p, use_container_width=True)
+for i, selected_file in enumerate(selected_files):
+    df1 = pd.read_csv(selected_file)
+    p = figure(x_axis_label=x_axis + '(wt.%)', y_axis_label=y_axis + '(wt.%)')
+    p.circle(df1[x_axis] / 10000, df1[y_axis] / 10000, color=colorlist[i])
+    plots.append(p)
+
+for plot in plots:
+    st.bokeh_chart(plot, use_container_width=True)
