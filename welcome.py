9c8ed1fedf3a52d94837d13883836a9a82ea9ae6
klarahei <141242034+klarahei@users.noreply.github.com>
2023-08-03 09:43:52 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import os
 
-file_name_list =[]
+file_name_list = []
 for i in os.listdir():
- if i.endswith('.csv')
+ if i.endswith('csv')
     file_name_list.append(i)
   
 st.write(file_name_list)
@@ -15,3 +15,5 @@ st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
+
+st.selectbox('select location', file_name_list)
