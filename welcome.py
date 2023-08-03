de86f0ee54084e21fe0b0ba2ba45d506488c6c88
klarahei <141242034+klarahei@users.noreply.github.com>
2023-08-03 12:42:48 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -15,14 +15,12 @@ st.write(file_name_list)
 df = pd.read_csv('Galapagos Islands.csv')
 st.dataframe(df)
 
-
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select x-element', el_list)
 y_axis = st.selectbox('select y-element', el_list)
 
 select_data = st.multiselect('select location', file_name_list)
 
-#
 colorlist = ['red','green','pink','yellow','blue']
 p = figure(x_axis_label = x_axis + '(wt.%)',y_axis_label = y_axis + '(wt.%)')
 for i in range(len(select_data)):
@@ -30,5 +28,4 @@ for i in range(len(select_data)):
  p.circle(df1[x_axis]/10000, df1[y_axis]/10000, color=colorlist[i])
 
 #show(p)
-
 st.bokeh_chart(p, use_container_width=True)
