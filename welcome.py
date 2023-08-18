1bef3663b6e52d0f6d868ebc405f563fdbbbe7bd
klarahei <141242034+klarahei@users.noreply.github.com>
2023-08-18 11:35:35 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -23,22 +23,8 @@ y_axis = st.selectbox('select y-element', el_list)
 select_data = st.multiselect('select data', file_name_list)
 #color definition
 
-color_exp1 = (0.5, 0, 0.5)    # Dark purple
-color_exp2 = (0, 0, 1)        # Blue
-color_exp3 = (0.5, 0.5, 1)    # Light blue
-color_exp4 = (0.47, 0.73, 0.43)  # Sage green
-color_exp5 = (0.5, 0.6, 0.2)  # Green
-color_exp6 = (0, 0.5, 0.1)  # Green
-color_exp7 = (1, 0.5, 0.1)  # Orange
-color_exp8 = (1, 0.5, 0.6)  # Light pink
-color_exp10 = (0.6, 0.3, 0.6)  # Purple
-color_exp11 = (0.9, 0.5, 0.7)  # Light pink
-color_exp12 = (0.3, 0.1, 0.5)  # Dark purple
-
-
 labellist = select_data
 colorlist = ['green', 'red','pink','yellow','blue','purple','orange']
-#colorlist = [color_exp1, color_exp2, color_exp3, color_exp4, color_exp5, color_exp6, color_exp7]
 p = figure(x_axis_label = x_axis + '(wt.%)',y_axis_label = y_axis + '(wt.%)')
 for i in range(len(select_data)):
  df1 = pd.read_csv(select_data[i], sep=r';')
